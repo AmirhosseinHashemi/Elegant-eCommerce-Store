65730b65b59215f51b9527a39463d46eefd3f719
Amirhossein <4mirh03einhashemi@gmail.com>
2023-12-14 23:31:05 +0330

    implement carousel functionality

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const menu = document.querySelector(".nav__menu");
 const menuList = document.querySelector(".nav__list");
 const arrowes = document.querySelectorAll(".nav__arrow");
 
+const carouselDots = document.querySelectorAll(".carousel__dot");
+const carouselSlides = document.querySelectorAll(".carousel__slide");
+
+const brandsLogo = document.querySelectorAll(".brands__logo");
+const brandsBtn = document.querySelectorAll(".brands__arrowes button");
+
+// handle mobile menu
 menuBtn.forEach((btn) =>
   btn.addEventListener("click", function () {
     menu.classList.toggle("open");
@@ -32,5 +39,16 @@ arrowes.forEach((icon) => {
     e.target.classList.toggle("rotate");
     const list = e.target.closest(".nav__item");
     list.querySelector(".nav__drop-down").classList.toggle("open");
+  });
+});
+
+// carousel
+carouselDots.forEach((dot) => {
+  dot.addEventListener("click", function (e) {
+    const dataDot = e.target.dataset.carouselDot - 1;
+    carouselSlides[dataDot].scrollIntoView({ behavior: "smooth" });
+
+    carouselDots.forEach((dot) => dot.classList.remove("active"));
+    e.target.classList.add("active");
   });
 });
